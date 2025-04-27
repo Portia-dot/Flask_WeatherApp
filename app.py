55361727e0081f7d6785f5957405171efd5519cd
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2025-04-27 09:24:17 -0700

    Current Weather and Error page implementation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from data import DataManager
 
 data_manager = DataManager()
@@ -8,12 +8,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    city_info = None
+    weather = None
     if request.method == 'POST':
         city = request.form.get('user_search')
         weather = data_manager.get_weather_by_city(city_name= city)
-        return render_template('index.html', city=city, weather=weather)
-    return render_template('index.html')
+
+        #City Info
+        city_info = {
+            'city': data_manager.city_name,
+            'state': data_manager.state,
+            'country': data_manager.country
+        }
+        if not weather:
+            return redirect('/error')
+        print(weather)
+    return render_template('index.html', city_info=city_info, weather=weather)
 
 
+@app.route('/error')
+def error():
+    return render_template('error.html')
 if __name__ == '__main__':
     app.run(debug=True)
--- data.py
+++ data.py
@@ -23,6 +23,15 @@ def fetch_data(api_url):
         return None
 
 
+def convert_kelvin_to_celsius(kelvin_temp):
+    return round(kelvin_temp - 273.15, 2)
+
+def wind_speed_to_kph(m_s):
+    return round(m_s * 3.6)
+
+def visibility_to_km(m):
+    return round(m / 1000, 2)
+
 class DataManager:
     def __init__(self):
         self.open_weather_api = None
@@ -35,6 +44,7 @@ class DataManager:
         self.exclude = 'current'
         self.state = None
         self.country = None
+        self.air_pollution_data = None
 
     def fetch_geocoding_data(self):
         self.geo_coding_url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.city_name}&limit={1}&appid={self.api_key}'
@@ -42,17 +52,17 @@ class DataManager:
         if not response:
             return None, None
         try:
+            self.city_name = response[0]['name']
             self.lat = response[0]['lat']
             self.lon = response[0]['lon']
-            self.state = response[0]['state'] if 'state' in response[0] else None
+            self.state = response[0].get('state')
             self.country = response[0]['country']
         except (IndexError, KeyError):
-            return None, None, None, None
-        return self.lat, self.lon, self.state, self.country
+            return None, None, None, None, None
+        return self.city_name, self.lat, self.lon, self.state, self.country
 
 
     def fetch_weather_data(self):
-
         self.open_weather_api =  f'https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&appid={self.api_key}'
         response = fetch_data(self.open_weather_api)
         if not response:
@@ -63,10 +73,46 @@ class DataManager:
             return None
         return self.weather_data
 
+    def get_air_pollution(self):
+        api_url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={self.lat}&lon={self.lon}&appid={self.api_key}'
+        response = fetch_data(api_url)
+        if not response:
+            return None
+        try:
+            air_pollution_data = response['list'][0]
+        except (IndexError, KeyError):
+            return None
+        return air_pollution_data
+
     def get_weather_by_city(self, city_name):
         self.city_name = city_name
         lat_lon = self.fetch_geocoding_data()
-        if not lat_lon :
+        if not lat_lon or not self.lat or not self.lon :
             return None
-        self.fetch_weather_data()
-        return self.weather_data
+        self.weather_data = self.fetch_weather_data()
+        self.air_pollution_data = self.get_air_pollution()
+        return self.get_current_weather()
+
+    def get_current_weather(self):
+        if not self.weather_data:
+            return None
+        return {
+            'temperature': convert_kelvin_to_celsius(self.weather_data['current']['temp']),
+            'humidity': self.weather_data['current']['humidity'],
+            'dew_point': convert_kelvin_to_celsius(self.weather_data['current']['dew_point']),
+            'pressure': self.weather_data['current']['pressure'],
+            'wind': wind_speed_to_kph(self.weather_data['current']['wind_speed']),
+            'feels_like': convert_kelvin_to_celsius(self.weather_data['current']['feels_like']),
+            'visibility': visibility_to_km(self.weather_data['current']['visibility']),
+            'air_quality': self.get_current_air_quality(),
+            'clouds': self.weather_data['current']['clouds'],
+            'uvi': self.weather_data['daily'][0]['uvi'],
+
+
+        }
+    def get_current_air_quality(self):
+        if not self.air_pollution_data:
+            return None
+        return {
+            'air_quality': self.air_pollution_data['main']['aqi']
+        }
